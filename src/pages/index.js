2bd8b59a7e1691fe72da9ea543c1dd844d9aec94
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2020-05-07 10:36:37 +0300

    finished my first gatsby blog task

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,50 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import "./index.css"
 
-const IndexPage = () => (
+export default ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <h1>Sviatlana's blog</h1>
+      <h3>Total count {data.allMarkdownRemark.totalCount}</h3>
+      {
+        data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link className="blog-link" to={node.fields.slug}>
+              <h4 className="title">
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </h4>
+            </Link>
+            <p>{node.excerpt}</p>
+          </div>
+        ))
+      }
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      edges {
+        node {
+          frontmatter {
+            date
+            description
+            title
+          }
+          excerpt
+          fields {
+            slug
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
